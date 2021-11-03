bbbc88d82edae237c9901f7abe778892e866c2e8
Khabz <khabubundivhu@gmail.com>
2021-11-04 00:02:15 +0200

    Add project files.

---
+++ Assessment/Services/ChuckNorriesService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Assessment.Services
+{
+    public class ChuckNorriesService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ChuckNorriesService(HttpClient client)
+        {
+            _httpClient = client;
+        }
+
+        public async Task<IList<string>> GetJokesCategoriesAsync()
+        {
+            var response = await _httpClient.GetAsync("jokes/categories");
+            response.EnsureSuccessStatusCode();
+            return await JsonSerializer.DeserializeAsync<IList<string>>(await response.Content.ReadAsStreamAsync());
+        }
+    }
+}
